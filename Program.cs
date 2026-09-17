a37394a57d222c49abab70cd287a6750391fc4e2
mennatarekk566 <mennatarek273994@gmail.com>
2026-09-17 06:45:34 +0300

    part01-question1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace G_ASP_NET_99_OOP_03
+﻿using System.Text;
+using System.Xml.Linq;
+
+namespace G_ASP_NET_99_OOP_03
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region first question 
+            //a)  What is the difference between Method Overloading and Method Overriding?
+
+            // Overloading means having multiple methods with the same name but different parameters,
+            //in the same class. It's decided at compile time.
+
+
+            //Overriding means a child class redefines a method it inherited from its parent,
+            //with the exact same signature.It's decided at run time
+
+            //b)  What is the difference between Static Binding and Dynamic Binding?
+             // Static binding is when the compiler decides which method to call at compile time
+             //Dynamic binding is when the method to call is decided at run time, based on the actual object
+         #endregion
         }
     }
 }
