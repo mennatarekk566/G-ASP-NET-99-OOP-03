1d14f544294d91e4cb64f21d67b3108afb0fcf2b
mennatarekk566 <mennatarek273994@gmail.com>
2026-09-17 06:38:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_ASP_NET_99_OOP_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
